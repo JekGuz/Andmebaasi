794159f4cc856281598264cce443f7e10ab8e53b
Catriin <jekaterina.guzek@gmail.com>
2025-01-30 11:33:31 +0200

    Update SQL_Server.sql
    Add TABLE NEW

--- SQL_Server.sql
+++ SQL_Server.sql
@@ -44,5 +44,66 @@ SELECT * FROM film;
 SELECT * FROM zanr;
 UPDATE film SET zanr_ID=5 WHERE fiml_ID=9
 
+---------------------------------------------
+USE TARgvGuzek;
+CREATE TABLE rezisoor( 
+  rezisoor_ID int PRIMARY KEY identity(1,1), 
+  nimi varchar(30), 
+  synniaeg date, 
+  riik varchar(15) 
+  );
 
+DROP TABLE rezisoor;
+
+SELECT * FROM rezisoor;
+
+INSERT INTO rezisoor(nimi, synniaeg, riik)
+VALUES('Jekaterina', '2000-11-04', 'Eesti'),
+('Mirra', '2001-09-06', 'London'),
+('Anne-ly', '2001-01-2', 'France'),
+('Norra', '2005-09-2', 'New-York');
+
+ALTER TABLE film ADD rezisoor_ID int;
+
+ALTER TABLE film ADD CONSTRAINT fk_rezisoor
+FOREIGN KEY (rezisoor_ID) REFERENCES rezisoor(rezisoor_ID);
+
+UPDATE film SET rezisoor_ID=1 WHERE fiml_ID=1
+
+UPDATE film SET rezisoor_ID=2 WHERE fiml_ID=7
+
+UPDATE film SET rezisoor_ID=3 WHERE fiml_ID=8
+
+UPDATE film SET rezisoor_ID=4 WHERE fiml_ID=9
+
+SELECT * FROM film;
+
+USE TARgvGuzek;
+CREATE TABLE kinokava( 
+  kinokava_ID int PRIMARY KEY identity(1,1), 
+  vaba_koht int,
+  kuupaev date
+  );
+
+INSERT INTO kinokava(vaba_koht, kuupaev)
+VALUES('5', '2025-01-30'),
+('10', '2025-01-30'),
+('25', '2025-01-30'),
+('75', '2025-01-30');
+
+ALTER TABLE kinokava ADD fiml_ID int;
+
+
+ALTER TABLE kinokava ADD CONSTRAINT fk_film
+FOREIGN KEY (fiml_ID) REFERENCES film(fiml_ID);
+
+UPDATE kinokava SET fiml_ID=1 WHERE kinokava_ID=1
+
+UPDATE kinokava SET fiml_ID=7 WHERE kinokava_ID=2
+
+UPDATE kinokava SET fiml_ID=8 WHERE kinokava_ID=3
+
+UPDATE kinokava SET fiml_ID=9 WHERE kinokava_ID=4
+
+SELECT * FROM kinokava;
 
